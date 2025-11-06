22d42adcd1da47bba644d0b97a2a7113f7f978cb
developmentAfterwork <developmentAfterwork@gmail.com>
2025-11-06 12:45:35 +0100

    fix AsureScans

--- Interpreter/Implementations/AsuraScans/AsureScansChapter.cs
+++ Interpreter/Implementations/AsuraScans/AsureScansChapter.cs
@@ -7,14 +7,11 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace ComicReader.Interpreter.Implementations.AsuraScans
-{
-	public class AsureScansChapter : BaseChapter, IChapter
-	{
+namespace ComicReader.Interpreter.Implementations.AsuraScans {
+	public class AsureScansChapter : BaseChapter, IChapter {
 		public AsureScansChapter(string? id, string source, string mangaName, string title, string homeUrl, string lastUpdate, TimeSpan timeout, IRequest requestHelper, HtmlHelper htmlHelper, INotification notification) : base(id, title, homeUrl, lastUpdate, mangaName, source, timeout, requestHelper, htmlHelper) { }
 
-		public override async Task<List<string>> ImplGetPageUrls()
-		{
+		public override async Task<List<string>> ImplGetPageUrls() {
 			var url = HomeUrl;
 			var response = await RequestHelper.DoGetRequest(url, 3, true, Timeout, RequestHeaders);
 
@@ -22,7 +19,7 @@ namespace ComicReader.Interpreter.Implementations.AsuraScans
 			var startIndex = response.IndexOf(startWord) + startWord.Length;
 			var part = response.Substring(startIndex);
 
-			var matches = Regex.Matches(part, "https[:\\/.a-z0-9\\-]*(.webp|.jpg)");
+			var matches = Regex.Matches(part, "https[:\\/.a-zA-Z0-9\\-]*(.webp|.jpg)");
 			var urls = new List<string>();
 
 			foreach (var match in matches.ToList()) {
