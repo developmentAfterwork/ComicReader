f1ee1a868f4746fa9383dea9455c81708790027d
developmentAfterwork <developmentAfterwork@gmail.com>
2025-11-06 12:45:51 +0100

    fix refresh

--- ComicReader/ViewModels/MangaDetailsViewModel.cs
+++ ComicReader/ViewModels/MangaDetailsViewModel.cs
@@ -12,10 +12,8 @@ using System.Collections.ObjectModel;
 using System.Windows.Input;
 using PopupService = ComicReader.Services.PopupService;
 
-namespace ComicReader.ViewModels
-{
-	public partial class MangaDetailsViewModel : ObservableObject
-	{
+namespace ComicReader.ViewModels {
+	public partial class MangaDetailsViewModel : ObservableObject {
 		private readonly InMemoryDatabase inMemoryDatabase;
 		private readonly Navigation navigation;
 		private readonly SettingsService settingsService;
@@ -79,8 +77,7 @@ namespace ComicReader.ViewModels
 		[ObservableProperty]
 		public ImageSource _coverUrlImageSource = default!;
 
-		public MangaDetailsViewModel(InMemoryDatabase database, Navigation navigation, SettingsService settingsService, MangaQueue mangaQueue, SimpleNotificationService simpleNotificationService, FileSaverService fileSaverService, Factory factory, RequestHelper requestHelper, PopupService popupService)
-		{
+		public MangaDetailsViewModel(InMemoryDatabase database, Navigation navigation, SettingsService settingsService, MangaQueue mangaQueue, SimpleNotificationService simpleNotificationService, FileSaverService fileSaverService, Factory factory, RequestHelper requestHelper, PopupService popupService) {
 			inMemoryDatabase = database;
 			this.navigation = navigation;
 			this.settingsService = settingsService;
@@ -101,16 +98,14 @@ namespace ComicReader.ViewModels
 			_initTask = new Task(async () => await Init());
 		}
 
-		public void OnAppearing()
-		{
+		public void OnAppearing() {
 			IsSearching = true;
 
 			_initTask = new Task(async () => await Init());
 			_initTask.Start();
 		}
 
-		private async Task Init()
-		{
+		private async Task Init() {
 			IManga manga = inMemoryDatabase.Get<IManga>("selectedManga");
 
 			if (_lastManga != null && manga == _lastManga) {
@@ -157,8 +152,7 @@ namespace ComicReader.ViewModels
 			IsSearching = false;
 		}
 
-		public async Task ChapterSelected(object? chapterObj)
-		{
+		public async Task ChapterSelected(object? chapterObj) {
 			if (SelectedItem != null) {
 				inMemoryDatabase.Set<IChapter>("selectedChapter", SelectedItem);
 				SelectedItem = null;
@@ -167,8 +161,7 @@ namespace ComicReader.ViewModels
 			}
 		}
 
-		public async Task AddBookmarkManga()
-		{
+		public async Task AddBookmarkManga() {
 
 			IManga manga = inMemoryDatabase.Get<IManga>("selectedManga");
 			try {
@@ -196,8 +189,7 @@ namespace ComicReader.ViewModels
 			}
 		}
 
-		public async Task OnDeleteManga()
-		{
+		public async Task OnDeleteManga() {
 
 			var result = await popupService.ShowPopupAsync("Delete Manga", "Do you want to delete that mange?", "Yes", "No");
 			if (!result) {
@@ -225,8 +217,7 @@ namespace ComicReader.ViewModels
 			}
 		}
 
-		public async Task OnDownloadMissingManga()
-		{
+		public async Task OnDownloadMissingManga() {
 			IManga manga = inMemoryDatabase.Get<IManga>("selectedManga");
 
 			IsSearching = true;
@@ -234,8 +225,7 @@ namespace ComicReader.ViewModels
 			IsSearching = false;
 		}
 
-		public async Task OnRefresh()
-		{
+		public async Task OnRefresh() {
 			IManga manga = inMemoryDatabase.Get<IManga>("selectedManga");
 
 			IsSearching = true;
@@ -253,8 +243,8 @@ namespace ComicReader.ViewModels
 							// check that all images are downloaded
 							var allImagesExists = mappedUrls.Values.All(f => File.Exists(f));
 							if (!allImagesExists) {
-								fileSaverService.DeleteChapterFile(chapterObj);
 								await fileSaverService.DeleteImagesFromChapter(chapterObj, factory);
+								fileSaverService.DeleteChapterFile(chapterObj);
 							}
 						}
 					}
@@ -263,11 +253,14 @@ namespace ComicReader.ViewModels
 				}
 			}
 
+			IManga reloadedManga = await factory.GetMangaFromBookmarkId(manga.GetUniqIdentifier());
+			inMemoryDatabase.Set<IManga>("selectedManga", reloadedManga);
+			await Init();
+
 			IsSearching = false;
 		}
 
-		private async Task OnOpen()
-		{
+		private async Task OnOpen() {
 			IManga manga = inMemoryDatabase.Get<IManga>("selectedManga");
 
 			await Browser.OpenAsync(manga.HomeUrl, BrowserLaunchMode.SystemPreferred);
