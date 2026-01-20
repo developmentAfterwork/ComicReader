92aedf3c41d7dd33350a8fb59bbdaf89cfc3660f
developmentAfterwork <developmentAfterwork@gmail.com>
2026-01-20 15:28:11 +0100

    Fix MangaKakalotManga

--- Interpreter/Implementations/MangaKakalot/MangaKakalotManga.cs
+++ Interpreter/Implementations/MangaKakalot/MangaKakalotManga.cs
@@ -105,7 +105,7 @@ namespace ComicReader.Interpreter.Implementations
 			var chaptersHtml = htmlHelper.ElementsByClass(response ?? string.Empty, "chapter-list").FirstOrDefault();
 
 			if (chaptersHtml is null) {
-				return new List<IChapter>();
+				return GetAlternative2(response);
 			}
 
 			var allChapterHtmls = htmlHelper.ElementsByClass(chaptersHtml, "row");
@@ -121,6 +121,41 @@ namespace ComicReader.Interpreter.Implementations
 			}
 
 			chapters.Reverse();
+
+			return chapters;
+		}
+
+		private List<IChapter> GetAlternative2(string? response)
+		{
+			var chaptersHtml = htmlHelper.ElementsByClass(response ?? string.Empty, "read-chapter").FirstOrDefault();
+
+			if (chaptersHtml is null) {
+				return new List<IChapter>();
+			}
+
+			var allChapterHtmls = htmlHelper.ElementsByTypeOuter(chaptersHtml, "a");
+
+			var chapters = new List<IChapter>();
+
+			if (allChapterHtmls.Count == 2) {
+				var chapterHtmlStart = allChapterHtmls[0];
+				var chapterHtmlEnd = allChapterHtmls[1];
+
+				var startNumberStr = htmlHelper.GetAttribute(chapterHtmlStart, "href").Substring(htmlHelper.GetAttribute(chapterHtmlStart, "href").LastIndexOf("-") + 1);
+				var endNumberStr = htmlHelper.GetAttribute(chapterHtmlEnd, "href").Substring(htmlHelper.GetAttribute(chapterHtmlEnd, "href").LastIndexOf("-") + 1);
+
+				var start = int.Parse(startNumberStr);
+				var end = int.Parse(endNumberStr);
+
+				var url = htmlHelper.GetAttribute(chapterHtmlStart, "href").Substring(0, htmlHelper.GetAttribute(chapterHtmlStart, "href").LastIndexOf("-") + 1);
+				for (int i = start; i <= end; i++) {
+					var u = url + i.ToString();
+					var title = u.Replace(HomeUrl, "").Substring(1);
+					chapters.Add(new MangaKakalotChapter(title, u, "...", Name, Source, timeout, requestHelper, htmlHelper));
+				}
+			} else {
+				return new List<IChapter>();
+			}
 
 			return chapters;
 		}
